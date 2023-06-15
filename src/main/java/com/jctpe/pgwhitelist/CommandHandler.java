ccc8bf097b2ce887b443d7b8fc1cb6adaab72237
Joe Tpe <chenyii1120@gmail.com>
2023-06-16 01:38:56 +0800

    list 指令實裝，目前先暫時 return google url

--- src/main/java/com/jctpe/pgwhitelist/CommandHandler.java
+++ src/main/java/com/jctpe/pgwhitelist/CommandHandler.java
@@ -16,19 +16,43 @@ public class CommandHandler implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         boolean senderIsPlayer = sender instanceof Player;
         String cmd = args[0];
+        Player tmpPlayer = null;
 
+        if (cmd.equalsIgnoreCase("help")) {
+            // return false 會被視為指令執行失敗，spigot 將會顯示指令說明訊息，因此在此處作為 help 內容顯示之用
+            return false;
+        }
+
+        if (cmd.equalsIgnoreCase("list")) {
+            sender.sendMessage("You can visit the web version whitelist: https://google.com/");
+            return true;
+        }
+
+        // 這段用來處理如果玩家沒有 OP 權限，就 show message and early return
+        if (senderIsPlayer){
+            tmpPlayer = (Player) sender;
+        }
+        // 此部分以上可用來執行無需 OP 權限也可執行的指令
+        if (tmpPlayer != null && !tmpPlayer.isOp()) {
+            sender.sendMessage(String.format("[PGWhitelist] %s", "Sorry, you don't have permission to do that."));
+            return true;
+        }
+
+        // 啟用白單
         if (cmd.equalsIgnoreCase("on")) {
             pluginSwitch.enablePlugin();
             sender.sendMessage("[PGWhitelist] Enabled!");
             return true;
         }
 
+        // 停用白單
         if (cmd.equalsIgnoreCase("off")) {
             pluginSwitch.disablePlugin();
             sender.sendMessage("[PGWhitelist] Disabled!");
             return true;
         }
 
+        // 將玩家加入白單
         if (cmd.equalsIgnoreCase("add")) {
             String rtn_msg = addPlayer.addPlayerToWhitelist(args[1], sender, senderIsPlayer);
             sender.sendMessage(String.format("[PGWhitelist] %s", rtn_msg));
